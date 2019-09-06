b1297d238a96a8fb33452e42c2aa01edaf54dd3f
Gabriel A <arruda.gabriel.tec@gmail.com>
2019-09-05 22:27:24 -0300

    divisao das notas corretamente

--- CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
+++ CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
@@ -1,24 +1,33 @@
-
 package caixaeletronico;
-
 
 public class CaixaEletronico {
 
-
     public static void main(String[] args) {
         int valor = 10;
-        int[] notas = new int[]{2,5,10,20,50,100}; 
-        
-        int qNotas = notas.length -1;
+        int[] notas = new int[]{2, 5, 10, 20, 50, 100};
+
+        int qNotas = notas.length - 1;
         for (int i = qNotas; i >= 0; i--) {
-            if (valor % notas[i] ==0) {
-                System.out.println(valor/notas[i] + 
-                        " x "+notas[i]);
-                
+            if (valor >= notas[i]) {
+                int notaAtual = valor / notas[i];
+                int resto = valor % notas[i];
+                System.out.println(valor / notas[i]
+                        + " x " + notas[i]);
+                if (resto > 0) {
+                    for (int j = i - 1; j >= 0; j--) {
+                        if (notas[j] <= resto) {
+                            notaAtual = resto / notas[j];
+                            System.out.println(notaAtual + " x " + notas[j]);
+                        }
+
+                    }
+                }
+                System.out.println("-------------");
+
             }
-            
+
         }
-         
+
     }
-    
+
 }
